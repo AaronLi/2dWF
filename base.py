5b952d9820e5d6229d6eaa2b2dfe128a06b5081a
Dumfing <dumpling.d.m@gmail.com>
2016-05-16 21:32:06 -0400

    Parkour Start
    Double jumps and Walls

--- base.py
+++ base.py
@@ -1,14 +1,15 @@
 #Base Platformer
-#TODO: Seperate walls and platforms, x y w h wall/plat
+#TODO: Seperate walls and platforms, x y w h wall(0)/plat(1)
 from pygame import *
 import glob
 class Mob:
     def __init__(self):
         self.X, self.Y=400,300
-        self.W, self.H= 16, 48
+        self.W, self.H= 16, 32
         self.vX, self.vY=0,0
         self.vM, self.vAx=4,0.3
         self.oG = False
+        self.jumps=2
 player=Mob()
 friction=0.1111
 gravity=0.25
@@ -16,19 +17,21 @@ currentTile=0
 tileSetRects=glob.glob('tileset/plat?.txt')
 drawTiles=[]
 tileRects=[]
+counter=0
 paused=False
 for i in range(len(tileSetRects)):
     tileFile=open(tileSetRects[i])
     tileRects.append([])
-    drawTiles.append(Surface((800,600)))
+    drawTiles.append(Surface((1280,720)))
     drawTiles[i].fill((255,255,255))
     for j in tileFile:
-        newPlat=[int(i) for i in j.split()]
+        platInfo=j.split()
+        newPlat=[int(platInfo[i]) for i in range(4)]
         draw.rect(drawTiles[i],(0,0,0),newPlat)
-        tileRects[i].append(Rect(newPlat))
-screen=display.set_mode((800,600))
+        tileRects[i].append([Rect(newPlat),int(platInfo[4])])
+screen=display.set_mode((1280,720))
 running=True
-playerStanding=Surface((16,48))
+playerStanding=Surface((player.W,player.H))
 playerStanding.fill((255,0,0))
 gameClock=time.Clock()
 onGround=False
@@ -62,36 +65,48 @@ def hitSurface(mob,tile):
     global gravity
     mobRect=Rect(mob.X,mob.Y,mob.W,mob.H)
     for platTile in tileRects[tile]:
-        if mobRect.move(0,1).colliderect(platTile):
+        if mobRect.move(0,1).colliderect(platTile[0]) and platTile[1]==0:
             mob.vY=0
-            if mobRect.bottom < platTile.bottom:
-                mob.Y=platTile.top-mob.H
+            if mobRect.bottom < platTile[0].bottom:
+                mob.Y=platTile[0].top-mob.H
                 mob.oG=True
-            elif mobRect.top>platTile.top:
-                mob.Y=platTile.bottom
+                mob.jumps=2
+            elif mobRect.top>platTile[0].top:
+                mob.Y=platTile[0].bottom
                 mob.oG=False
-            else:
-                mob.Y=platTile.top-mob.H
-        if not mobRect.move(0,0).colliderect(platTile):
+        if mobRect.colliderect(platTile[0]) and platTile[1]==1:
+            if mobRect.left < platTile[0].left:
+                mob.X=platTile[0].left-mob.W-1
+                print('l')
+                mob.jumps=2
+            elif mobRect.right>platTile[0].right:
+                mob.X=platTile[0].right+1
+                print('r')
+                mob.jumps=2
+        if not mobRect.colliderect(platTile[0]):
             mob.oG=False
     if not mob.oG:
         mob.vY+=gravity
 def drawStuff(tileNum):
     screen.blit(drawTiles[tileNum],(0,0))
-    screen.blit(playerStanding,(int(player.X)-8, int(player.Y)))
+    screen.blit(playerStanding,(int(player.X), int(player.Y)))
 while running:
     for e in event.get():
         if e.type==QUIT:
             running=False
         if e.type==KEYDOWN:
-            if e.key ==K_w:
+            if e.key ==K_w and player.jumps>0:
                 player.vY=-6
                 player.oG=False
+                player.jumps-=1
             if e.key==K_p:
                 if paused:
                     paused=False
                 elif not paused:
                     paused=True
+            if e.key==K_c:
+                counter+=1
+                currentTile=counter%len(drawTiles)
     display.set_caption(str(int(gameClock.get_fps())))
     if not paused:
         keysDown()
