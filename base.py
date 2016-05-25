56c8f07bf2b7308d4e91891fd7121351f35fdc01
Dumfing <dumpling.d.m@gmail.com>
2016-05-25 14:00:39 -0400

    Now we begin random level generation

--- base.py
+++ base.py
@@ -3,7 +3,7 @@
 #Have a master platform list that contains all platforms with preoffset platforms in them?
 #Stitch together all platform visuals?
 from pygame import *
-import glob,pprint
+import glob,pprint,random
 class Mob:
     def __init__(self):
         self.X, self.Y=400,300
@@ -11,11 +11,28 @@ class Mob:
         self.vX, self.vY=0,0
         self.vM, self.vAx=4,0.3
         self.oG, self.oW = False, False
+        self.fA = False #(player.fAcing) True for right, False for left
         self.jumps=2
-player=Mob()
-frame=0
-animation=0
 idleRight, idleLeft, right, left, jumpRight, jumpLeft = 0, 1, 2, 3, 4, 5
+player=Mob()
+currentFrame=0
+#Animations
+frames=[[[image.load("images/frost001.png")],1],[[image.load("images/frost003.png"),image.load("images/frost004.png"),image.load("images/frost005.png"),image.load("images/frost006.png"),image.load("images/frost007.png"),image.load("images/frost008.png")],5],[[image.load("images/frost015.png"),image.load("images/frost016.png"),image.load("images/frost017.png"),image.load("images/frost018.png"),image.load("images/frost019.png"),image.load("images/frost020.png"),image.load("images/frost021.png")],5]]
+flippedFrame=Surface((0,0))
+ogFrames=[0,2,4]
+flipFrameOrder=[1,3,5]
+for i in range(len(frames)):
+    addToFrameList=[]#Frames and how fast to play them
+    flipFrameList=[]#Just the frames
+    workFrame=ogFrames[i]
+    for j in range(len(frames[workFrame][0])):
+        flippedFrame=transform.flip(frames[workFrame][0][j],True,False)
+        flipFrameList.append(flippedFrame)
+    addToFrameList.append(flipFrameList) 
+    addToFrameList.append(frames[workFrame][1])
+    frames.insert(flipFrameOrder[i],addToFrameList)
+    
+animation=0
 idle=idleRight
 jump=jumpRight
 friction=0.2111
@@ -40,44 +57,24 @@ for i in range(len(tileSetRects)):
         newPlat=[int(platInfo[i]) for i in range(4)]
         draw.rect(drawTiles[i],(0,int(platInfo[4])*255,0),newPlat)
         tileRects[i].append([Rect(newPlat),int(platInfo[4])])
-pprint.pprint(tileSizes)
-pprint.pprint(tileRects)
 screen=display.set_mode((1280,720))
 running=True
 playerStanding=Surface((player.W,player.H))
 playerStanding.fill((255,0,0))
 gameClock=time.Clock()
 onGround=False
-def keysDown():
-    global player, animation, idle, jump, frame
-    keys=key.get_pressed()
-    animation=idle
-    if player.oG==False:
-        animation=jump
+def keysDown(keys):
+    global player
     if keys[K_a]:
         player.vX-=player.vAx
-        idle = idleLeft
-        jump=jumpLeft
-        if player.oG==True:
-            animation = left
+        player.fA=True
     if keys[K_s]:
         print("Insert Crouch Thing")
     if keys[K_d]:
         player.vX+=player.vAx
-        idle = idleRight
-        jump= jumpRight
-        if player.oG==True:
-            animation = right
-    if animation!=idle:   
-        frame = frame + 0.15 
-        if frame >= len(pics[animation]):
-            frame = 0
-    if animation==idle:
-        frame=0
-        
+        player.fA=False
 def applyFriction(mob):
     global friction
-    print(mob.oG)
     if mob.vX>0:
         if mob.oG:
             #Friction on Ground
@@ -126,34 +123,38 @@ def hitSurface(mob,tile):
             wallRect=platTile
             if mobRect.left < platTile[0].left:
                 mob.X=platTile[0].left-mob.W-1
-                mob.jumps=2
+                mob.jumps=1
             elif mobRect.right>platTile[0].right:
                 mob.X=platTile[0].right+1
-                mob.jumps=2
+                mob.jumps=1
     if not mobRect.move(0,1).colliderect(hitRect[0]):
         mob.oG=False
         mob.vY+=gravity
     if not (mobRect.move(0,1).colliderect(wallRect[0]) or mobRect.move(0,-1).colliderect(wallRect[0])):
         mob.oW=False
-def aniPics(name,start,end):
-    pics = []
-    for i in range(start,end+1):
-        picture=image.load("Images/%s%03d.png" % (name,i))
-        picture=transform.scale(picture,(int(picture.get_width()*1.5),int(picture.get_height()*1.5)))
-        pics.append(picture)
-    return pics
-pics = []
-pics.append(aniPics("Excalibur",1,1))
-pics.append(aniPics("Excalibur",2,2))
-pics.append(aniPics("Excalibur",3,8))
-pics.append(aniPics("Excalibur",9,14))
-pics.append(aniPics("Excalibur",15,21))
-pics.append(aniPics("Excalibur",22,28))
-def drawStuff(tileNum):
-    pic = pics[animation][int(frame)]
+
+def drawStuff(tileNum,keys):
+    global currentFrame,frames,animation
+    if keys[K_a] and player.oG:
+        animation=left
+    elif keys[K_d] and player.oG:
+        animation=right
+    elif player.fA and not player.oG:
+        animation=jumpLeft
+    elif not player.fA and not player.oG:
+        animation=jumpRight
+    elif player.fA and player.oG:
+        animation=idleLeft
+    elif not player.fA and player.oG:
+        animation=idleRight
+    #print(frames[animation][1],currentFrame,currentFrame//frames[animation][1]%len(frames[animation][0]))
+    pic = frames[animation][0][currentFrame//frames[animation][1]%len(frames[animation][0])]
+    currentFrame+=1
     screen.fill((0,0,0))
     screen.blit(drawTiles[tileNum],(tileSizes[tileNum][0]//2-player.X,tileSizes[tileNum][1]//2-player.Y))
     screen.blit(pic,(640,360))
+def makeNewLevel(levelLength):
+    levelOut=[]
 while running:
     for e in event.get():
         if e.type==QUIT:
@@ -172,12 +173,13 @@ while running:
                 counter+=1
                 currentTile=counter%len(drawTiles)
     display.set_caption(str(int(gameClock.get_fps())))
+    keysIn=key.get_pressed()
     if not paused:
-        keysDown()
+        keysDown(keysIn)
         move(player)
         hitSurface(player,currentTile)
         player=applyFriction(player)
-        drawStuff(currentTile)
+        drawStuff(currentTile,keysIn)
     display.flip()
     gameClock.tick(60)
 quit()
